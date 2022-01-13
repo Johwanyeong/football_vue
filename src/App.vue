4ef3b4423bf38cbbaf0f82eb3baa7dae5a27bd0b
Johwanyeong <85853166+Johwanyeong@users.noreply.github.com>
2022-01-13 16:24:40 +0900

    20220113
    권한에 따른 메뉴바 노출

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,11 @@
 <template>
   <div id="nav">
     <router-link to="/">home</router-link>|
-    <router-link to="/Join">Join</router-link>|
-    <router-link to="/Login">Login</router-link>|
-    <router-link to="/Logout">Logout</router-link>|
-    <router-link to="/Registration">Registration</router-link>|
+    <router-link v-if="!logged" to="/Join">Join</router-link>|
+    <router-link v-if="!logged" to="/Login">Login</router-link>|
+    <router-link v-if="logged" to="/Logout">Logout</router-link>|
+    <router-link v-if="adminLogged" to="/Registration">Registration</router-link>|
+
     <hr />
     <router-view />
   </div>
@@ -18,10 +19,17 @@ export default {
   methods: {
     changeLogged() {
       const token  = sessionStorage.getItem("TOKEN");
-      if(token != null) {
+      const role = sessionStorage.getItem("ROLE");
+      if(token != null) { // 로그인 되었을 경우
         this.logged =true;
+        if(role === "ADMIN"){ // 로그인 후 role이 admin일 경우
+          this.adminLogged = true;
+        }
+        else{ //로그인 후 role이 admin이 아닐 경우
+          this.adminLogged = false;
+        }
       }
-      else{
+      else{ // 로그인 되지 않았을 경우
         this.logged = false;
       }
     },
