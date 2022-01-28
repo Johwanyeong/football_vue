85e9f6a39dc3ad7932581fe2af55cd9afed80365
Johwanyeong <85853166+Johwanyeong@users.noreply.github.com>
2022-01-28 12:22:07 +0900

    20220128
    스카우터 목록 삭제

--- src/components/ScoutList.vue
+++ src/components/ScoutList.vue
@@ -11,6 +11,7 @@
                 <td>국적</td>
                 <td>포지션</td>
                 <td>몸 값</td>
+                <td>삭제</td>
             </tr>
             <tr v-for="scoutlist in scoutlists" v-bind:key="scoutlist">
                 <td>{{scoutlist.scoutno}}</td>
@@ -19,6 +20,8 @@
                 <td>{{scoutlist.player.playercountry}}</td>
                 <td>{{scoutlist.player.playerposition}}</td>
                 <td>{{scoutlist.player.playerprice}}</td>
+                <td><el-button @click="handleDelete" type="danger">
+                    <input type="hidden" v-model="sno" >{{scoutlist.scoutno}}</el-button></td>
             </tr>
         </table>
         <el-pagination 
@@ -42,17 +45,24 @@
                 const headers = {"Content-Type":"application/json",
                     token : this.token}
                 const response = await axios.get(url, {headers});
-                console.log(response);
+                // console.log(response);
                 this.scoutlists = response.data.scoutlist;
  
                 const url1 = `REST/scoutcount` //스카우트 숫자 조회 주소 입력
                 const response1 = await axios.get(url1, {headers}); //userid를 찾기 위해 토큰 사용
-                console.log(response1);
+                // console.log(response1);
                 //스카우트 숫자를 통해 페이지네이션 숫자 생성
                 this.totalpage = Number(response1.data.count);
             },
             async nextPage(playerno) {
                 this.$router.push({name: 'Player_One', query:{no:playerno}});
+            },
+            async handleDelete(){
+                const url = `/REST/scoutdelete?sno=${this.sno}`;
+                const headers = {"Content-Type":"application/json",
+                    token : this.token};
+                const response = await axios.delete(url,{headers});
+                console.log(response);
             }
         },
         async created(){
@@ -65,7 +75,10 @@
                 page : '',
                 scoutlists : [],
                 player: [],
-                totalpage : ''
+                totalpage : '',
+                text : '',
+
+                sno : ''
             }
         }
     }
